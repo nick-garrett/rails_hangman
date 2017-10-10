65d5a95af5abaf21e257245eb170bda6b3f9fbb6
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-11 09:38:17 +1300

    Seeds words from the dictionary file

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,4 @@
 # encoding: UTF-8
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171009195015) do
+ActiveRecord::Schema.define(version: 20171010200411) do
 
   create_table "games", force: :cascade do |t|
     t.string   "word"
@@ -31,6 +30,10 @@ ActiveRecord::Schema.define(version: 20171009195015) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "words", force: :cascade do |t|
+    t.string "word"
   end
 
 end
