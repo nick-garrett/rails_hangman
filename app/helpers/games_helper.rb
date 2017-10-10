8962131a156c95abf0a8ce3cc8d32de2896a483e
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-11 08:33:52 +1300

    Abstracted some things from views into helper methods.

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,21 @@
 module GamesHelper
+  def start_button
+    button_to('Start Game', '/games', :autofocus => true)
+  end
+
+  def masked_word
+    @game.masked_word.map { |x| x ? x : '_' }.join(' ')
+  end
+
+  def guessed_letters
+    @game.letters.join
+  end
+
+  def lives_remaining
+    @game.lives_remaining
+  end
+
+  def game_running?
+    !@game.game_over?
+  end
 end
