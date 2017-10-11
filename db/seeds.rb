87fce4a9573d13515b21570a7066e9dc3339b41a
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-11 13:19:24 +1300

    General tidying

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 Word.destroy_all
 File.readlines(Rails.root + 'config/dictionary.txt').each { |cur_word| Word.create(word: cur_word) }
