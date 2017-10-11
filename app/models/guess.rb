0ea5aed72eb97b9dfdf4e139282be5e492fbf153
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-11 13:17:07 +1300

    Added messages to validations so can be displayed to the user. add a before validation hook that downcases the letter.

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,8 +1,14 @@
 #:nodoc:
 class Guess < ActiveRecord::Base
   # attr_accessor :letter, :game_id
-  validates :letter, uniqueness: { scope: :game_id }
-  validates_format_of :letter, with: /\A[a-zA-Z]*\z/
+  validates_uniqueness_of :letter, scope: :game_id, message: 'Already guessed this letter'
+  validates_format_of :letter, with: /\A[a-zA-Z]*\z/, message: 'Guess must be a letter'
+
+  before_validation :downcase_letter
 
   belongs_to :game
+
+  def downcase_letter
+    letter.downcase!
+  end
 end
