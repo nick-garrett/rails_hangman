9a9796ccd85bb42e88ab1c02087619e78ddeb83f
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-12 13:46:46 +1300

    General tidying of filesystem.

--- app/helpers/guesses_helper.rb
+++
@@ -1,3 +0,0 @@
-#:nodoc:
-module GuessesHelper
-end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,55 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  let(:game) { Game.new }
+  let(:word) { 'word' }
+
+  describe '#letters' do
+    # Should I actually test this???
+    pending 'Not sure if this is testable, or if it should be tested'
+  end
+
+  describe '#game_over?' do
+    context 'When the game is not lost' do
+      before do
+        allow(game).to receive(:lost?).and_return(false)
+      end
+
+      context 'when the game is won' do
+        before do
+          allow(game).to receive(:letters).and_return(word.chars)
+          allow(game).to receive(:word).and_return(word)
+        end
+
+        it 'is game over' do
+          expect(game).to be_game_over
+        end
+      end
+
+      context 'when the game is not won' do
+        before do
+          allow(game).to receive(:guessed).and_return(word.chars)
+        end
+
+        it 'is not game over' do
+          expect(game).not_to be_game_over
+        end
+      end
+    end
+  end
+
+  describe '#lives_remaining' do
+    pending 'lives_remaining tests'
+  end
+
+  describe '#masked_word' do
+    pending 'masked_word tests'
+  end
+
+  describe '#end_message' do
+    pending 'end_message test'
+  end
 end
