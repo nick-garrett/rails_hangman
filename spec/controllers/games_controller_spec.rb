4999376407b6710fdc5923627b275c6b5e174fbf
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-13 11:03:04 +1300

    Wrote first test. This test describes GET #new and checks that it returns an http success status.

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -4,4 +4,11 @@ require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'GET #new' do
+    it 'returns http success' do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
