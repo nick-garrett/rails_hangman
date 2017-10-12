f22d10b8ef9557175a4e028e8e0b0f027b657562
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-12 13:45:51 +1300

    Now calls chomp when creating new word in the database, so it doesn't have to be called elsewhere.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 Word.destroy_all
-File.readlines(Rails.root + 'config/dictionary.txt').each { |cur_word| Word.create(word: cur_word) }
+File.readlines(Rails.root + 'config/dictionary.txt').each { |cur_word| Word.create(word: cur_word.chomp) }
