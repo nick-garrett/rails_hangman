20539828ac27f2c6eaedb70bffcd73cf19fb8232
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-13 11:32:37 +1300

    Edited first test to be testing #index, not #new. Also added Post #create test, testing that it redirects correctly

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -3,12 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
-  pending "add some examples to (or delete) #{__FILE__}"
-
-  describe 'GET #new' do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  fixtures 'words'
+  
+  describe 'GET #index' do
     it 'returns http success' do
-      get :new
+      get :index
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'POST #create' do
+    it 'redirects to show action' do
+      post :create
+      expect(response).to redirect_to(assigns[:game])
     end
   end
 end
