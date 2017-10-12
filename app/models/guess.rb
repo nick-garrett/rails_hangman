4c1ffaf9f5b0d920a18a1715e76329f441fb7df5
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-12 13:36:28 +1300

    Made note to test if scope is required in this instance

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,6 +1,6 @@
 #:nodoc:
 class Guess < ActiveRecord::Base
-  # attr_accessor :letter, :game_id
+  # TODO: test if it works without the scope
   validates_uniqueness_of :letter, scope: :game_id, message: 'Already guessed this letter'
   validates_format_of :letter, with: /\A[a-zA-Z]+\z/, message: 'Guess must be a letter'
 
