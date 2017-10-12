5fde37b109324ce8fd0f77cd32f98c5a17e6b33a
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-13 10:47:39 +1300

    Added tests for checking that the validation is working correctly, as well as the before validation hook runs the downcase method on letters.

--- spec/models/guess_spec.rb
+++ spec/models/guess_spec.rb
@@ -3,5 +3,63 @@
 require 'rails_helper'
 
 RSpec.describe Guess, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#letter' do
+    context 'when letter has invalid format' do
+      let(:guess) { Guess.new }
+
+      before do
+        guess.letter = '1'
+      end
+
+      it "doesn't pass validation checks" do
+        expect(guess).not_to be_valid
+        expect(guess.errors[:letter]).to include 'Guess must be a letter'
+      end
+    end
+
+    context 'when letter has already been guessed' do
+      let(:guess) { Guess.new }
+
+      before do
+        guess_two = Guess.new
+        guess_two.letter = 'a'
+        guess_two.game_id = 1
+        guess_two.save
+        guess.letter = 'a'
+        guess.game_id = 1
+      end
+
+      it "doesn't pass validation checks" do
+        expect(guess).not_to be_valid
+        expect(guess.errors[:letter]).to include 'Already guessed this letter'
+      end
+    end
+
+    context 'when letter has correct format and has not been guessed' do
+      let(:guess) { Guess.new }
+
+      before do
+        guess.letter = 'a'
+      end
+
+      it 'passes validation checks' do
+        expect(guess).to be_valid
+        expect(guess.errors[:letter]).not_to include 'Guess must be a letter'
+        expect(guess.errors[:letter]).not_to include 'Already guessed this letter'
+      end
+    end
+
+    context 'when capital letter is guessed' do
+      let(:guess) { Guess.new }
+      let(:letter) { 'A' }
+
+      before do
+        guess.letter = letter
+        guess.valid?
+      end
+      it 'downcases before validation' do
+        expect(guess.letter).to eq letter.downcase
+      end
+    end
+  end
 end
