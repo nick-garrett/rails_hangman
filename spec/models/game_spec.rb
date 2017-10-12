f8e652b07e0a5292b547210c10295dd47acab8df
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-13 10:01:18 +1300

    Finished writing spec file. Testing all methods, using fixtures.

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -3,53 +3,111 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
-  let(:game) { Game.new }
+  fixtures :games
+  fixtures :guesses
+
   let(:word) { 'word' }
 
-  describe '#letters' do
-    # Should I actually test this???
-    pending 'Not sure if this is testable, or if it should be tested'
+  describe '#guessed_letters' do
+    context 'when no letters are guessed' do
+      let(:game) { games(:no_guess_game) }
+
+      it 'should return an empty array' do
+        expect(game.guessed_letters).to be_empty
+      end
+    end
+
+    context 'when some letters are guessed' do
+      let(:game) { games(:word_guessed_game) }
+
+      it 'should return a non empty array' do
+        expect(game.guessed_letters).not_to be_empty
+      end
+
+      it 'should return the correct letters in the array' do
+        expect(game.guessed_letters).to eq %w[w o r d]
+      end
+    end
   end
 
-  describe '#game_over?' do
-    context 'When the game is not lost' do
-      before do
-        allow(game).to receive(:lost?).and_return(false)
+  describe '#won?' do
+    context 'when all of the word letters are guessed' do
+      let(:game) { games(:won_game) }
+
+      it 'is won' do
+        expect(game).to be_won
       end
+    end
 
-      context 'when the game is won' do
-        before do
-          allow(game).to receive(:letters).and_return(word.chars)
-          allow(game).to receive(:word).and_return(word)
-        end
+    context 'when none of the letters are guessed' do
+      let(:game) { games(:not_won_game) }
 
-        it 'is game over' do
-          expect(game).to be_game_over
-        end
+      it 'is not won' do
+        expect(game).not_to be_won
       end
+    end
+  end
 
-      context 'when the game is not won' do
-        before do
-          allow(game).to receive(:guessed).and_return(word.chars)
-        end
+  describe '#lost?' do
+    context 'when lives_remaining is zero' do
+      let(:game) { games(:lost_game) }
 
-        it 'is not game over' do
-          expect(game).not_to be_game_over
-        end
+      it 'is lost' do
+        expect(game).to be_lost
+      end
+    end
+
+    context 'when lives_remaining is > 0' do
+      let(:game) { games(:not_lost_game) }
+
+      it 'is not lost' do
+        expect(game).not_to be_lost
       end
     end
   end
 
   describe '#lives_remaining' do
-    pending 'lives_remaining tests'
+    # pending 'lives_remaining tests'
+    context 'when number of incorrect guesses is the same as total lives' do
+      let(:game) { games(:zero_lives_game) }
+
+      it 'returns 0' do
+        expect(game.lives_remaining).to eq 0
+      end
+    end
+
+    context 'when number of incorrect guesses is the less than as total lives' do
+      let(:game) { games(:nine_lives_game) }
+
+      it 'returns greater than zero' do
+        expect(game.lives_remaining).to be > 0
+      end
+    end
   end
 
   describe '#masked_word' do
-    pending 'masked_word tests'
-  end
+    context 'when all of the word is guessed' do
+      let(:game) { games(:won_game) }
 
-  describe '#end_message' do
-    pending 'end_message test'
+      it 'returns the letters of the word' do
+        expect(game.masked_word).to eq word.chars
+      end
+    end
+
+    context 'when some of the word is guessed' do
+      let(:game) { games(:first_letter_guessed_game) }
+
+      it 'returns a nil array with the guessed letters from the word inserted' do
+        expect(game.masked_word).to eq [word.chars.first, nil, nil, nil]
+      end
+    end
+
+    context 'when only wrong letters are guessed' do
+      let(:game) { games(:lost_game) }
+
+      it 'returns a nil array' do
+        expect(game.masked_word).to eq [nil, nil, nil, nil]
+      end
+    end
   end
 end
