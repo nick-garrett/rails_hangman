5d53b88112740278f7383f6c7b5c53dd44d1db49
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-13 13:34:33 +1300

    Added tests for GET #show. includes http_success messages, setting game object, and rendering the complete template. Next to add check if id does not exist.

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -5,7 +5,8 @@ require 'rails_helper'
 RSpec.describe GamesController, type: :controller do
   # pending "add some examples to (or delete) #{__FILE__}"
   fixtures 'words'
-  
+  fixtures 'games'
+
   describe 'GET #index' do
     it 'returns http success' do
       get :index
@@ -17,6 +18,28 @@ RSpec.describe GamesController, type: :controller do
     it 'redirects to show action' do
       post :create
       expect(response).to redirect_to(assigns[:game])
+    end
+
+    it 'creates a new game object' do
+      post :create
+      expect(assigns[:game]).not_to be_nil
+    end
+  end
+
+  describe 'GET #show' do
+    it 'returns http success' do
+      get :show, id: 1
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'sets game object' do
+      get :show, id: 1
+      expect(assigns[:game]).not_to be_nil
+    end
+
+    it 'renders the show template' do
+      get :show, id: 1
+      expect(response).to render_template(:show)
     end
   end
 end
