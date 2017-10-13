e2a0615cc5abda2bf40419ecb77beaa640248694
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-13 13:59:53 +1300

    Changed to find_by so it doesnt throw an error if the game doesn't exist. Addded a check for nil game and redirects back to index if it is.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,6 +9,7 @@ class GamesController < ApplicationController
   end
 
   def show
-    @game = Game.find(params[:id])
+    @game = Game.find_by(id: params[:id])
+    redirect_to action: "index" if !@game
   end
 end
