1b8d04443310569f5ea39cc8fbf1ad9c2f4f1ce7
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-13 15:16:08 +1300

    Moved before blocks into context blocks so that it is easier to change what is being posted. Added test for when an invalid game_id is posted, checks that it is redirected to root.

--- spec/controllers/guesses_controller_spec.rb
+++ spec/controllers/guesses_controller_spec.rb
@@ -10,13 +10,13 @@ RSpec.describe GuessesController, type: :controller do
     let(:guess) { 'a' }
     let(:game) { games(:won_game) }
 
-    before do
-      post :create, game_id: game.id, guess: { letter: guess }
-    end
-
     context 'when a valid game_id and valid guess is posted' do
       let(:guess) { 'a' }
       let(:game) { games(:won_game) }
+
+      before do
+        post :create, game_id: game.id, guess: { letter: guess }
+      end
 
       it 'redirects to game' do
         expect(response).to redirect_to(game)
@@ -31,12 +31,26 @@ RSpec.describe GuessesController, type: :controller do
       let(:guess) { '!' }
       let(:game) { games(:won_game) }
 
+      before do
+        post :create, game_id: game.id, guess: { letter: guess }
+      end
+
       it 'redirects to game' do
         expect(response).to redirect_to(game)
       end
 
       it 'has errors' do
         expect(flash[:error]).not_to be_nil
+      end
+    end
+
+    context 'when an invalid game_id is posted' do
+      before do
+        post :create, game_id: 10000, guess: { letter: guess }
+      end
+
+      it 'redirects to root' do
+        expect(response).to redirect_to('/')
       end
     end
   end
