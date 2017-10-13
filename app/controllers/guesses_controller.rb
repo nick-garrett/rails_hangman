4994880c25a64547a9670e1497dd74a083acc704
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-13 14:19:59 +1300

    Changed to find_by so that it will not throw an error if there is no game with the given id. Added a nil check for game that redirects to root if the game doesn't exist.

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -3,8 +3,8 @@
 #:nodoc:
 class GuessesController < ApplicationController
   def create
-    # TODO: what happens when it doesn't exist, find_by, find_by_id (don't use), find
-    game = Game.find(params[:game_id])
+    game = Game.find_by(id: params[:game_id])
+    redirect_to '/' unless game
     guess = game.guesses.create(guess_params)
     # TODO: flash conventions, full_messages
     flash[:error] = guess.errors.first.last unless guess.save
