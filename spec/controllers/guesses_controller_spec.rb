008c8974283b4d5844fcf5072360685fe5b687a9
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-13 14:54:13 +1300

    Add test for when an invalid guess is made. Makes sure the redirect still happens, but flash[:error] is not empty

--- spec/controllers/guesses_controller_spec.rb
+++ spec/controllers/guesses_controller_spec.rb
@@ -3,16 +3,40 @@
 require 'rails_helper'
 
 RSpec.describe GuessesController, type: :controller do
+  fixtures :games
+  fixtures :guesses
+
   describe 'POST #create' do
     let(:guess) { 'a' }
+    let(:game) { games(:won_game) }
 
     before do
-      post :create, game_id: 1, guess: [letter: guess]
+      post :create, game_id: game.id, guess: { letter: guess }
     end
 
     context 'when a valid game_id and valid guess is posted' do
+      let(:guess) { 'a' }
+      let(:game) { games(:won_game) }
+
       it 'redirects to game' do
-        expect(response).to redirect_to(assigns[:game])
+        expect(response).to redirect_to(game)
+      end
+
+      it 'has no errors' do
+        expect(flash[:error]).to be_nil
+      end
+    end
+
+    context 'when a valid game_id and an invalid guess is posted' do
+      let(:guess) { '!' }
+      let(:game) { games(:won_game) }
+
+      it 'redirects to game' do
+        expect(response).to redirect_to(game)
+      end
+
+      it 'has errors' do
+        expect(flash[:error]).not_to be_nil
       end
     end
   end
