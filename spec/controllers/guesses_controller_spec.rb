d10e68b2c352aa2af05a5f1075e35c6d83041e15
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-13 14:37:23 +1300

    Add test to check that the correct redirect happens if a valid guess and game_id is posted to guess #create

--- spec/controllers/guesses_controller_spec.rb
+++ spec/controllers/guesses_controller_spec.rb
@@ -3,5 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe GuessesController, type: :controller do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'POST #create' do
+    let(:guess) { 'a' }
+
+    before do
+      post :create, game_id: 1, guess: [letter: guess]
+    end
+
+    context 'when a valid game_id and valid guess is posted' do
+      it 'redirects to game' do
+        expect(response).to redirect_to(assigns[:game])
+      end
+    end
+  end
 end
