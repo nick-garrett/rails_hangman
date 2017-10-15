8708ff69071a6f36fdbbdb2b3dba718660d1a3d1
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-16 11:26:11 +1300

    Style fix.

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,7 +9,7 @@ require 'cucumber/rails'
 Before do
   ActiveRecord::FixtureSet.reset_cache
   fixtures_folder = File.join(Rails.root, 'spec', 'fixtures')
-  fixtures = Dir[File.join(fixtures_folder, '*.yml')].map {|f| File.basename(f, '.yml') }
+  fixtures = Dir[File.join(fixtures_folder, '*.yml')].map { |f| File.basename(f, '.yml') }
   ActiveRecord::FixtureSet.create_fixtures(fixtures_folder, fixtures)
 end
 
