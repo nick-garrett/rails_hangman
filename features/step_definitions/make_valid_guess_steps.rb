30b27712166797d666c1277290779ff082de5a8a
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-16 10:48:48 +1300

    Remove the Then because it is a duplicate from another step file

--- features/step_definitions/make_valid_guess_steps.rb
+++ features/step_definitions/make_valid_guess_steps.rb
@@ -1,5 +1,3 @@
-# let(:guess) { 't' }
-
 Given(/^I have started a new game$/) do
   visit root_path
   click_button('Start Game')
@@ -8,8 +6,4 @@ end
 When(/^I submit the guess "(.)"$/) do |guess|
   fill_in 'guess[letter]', with: guess
   click_button('Submit')
-end
-
-Then(/^I should see the guess added to the list of guessed letters$/) do
-  expect(page).to have_content('Guessed Letters: t')
 end
