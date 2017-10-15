38f6d271911f39932ef90155071d6d348bf7ab9b
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-16 12:08:10 +1300

    Checks if there are no elements in the database and returns nil if there aren't any. Need to update so that it doesn't call random_order twice.

--- app/models/word.rb
+++ app/models/word.rb
@@ -3,6 +3,7 @@ class Word < ActiveRecord::Base
   scope :random_order, -> { order('RANDOM()') }
 
   def self.choose_word
+    return nil if Word.random_order.empty?
     Word.random_order.first.word
   end
 end
