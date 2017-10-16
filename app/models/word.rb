4660805fbf058893a3dea741e4348a83a104f138
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-16 14:24:40 +1300

    Using &.word will return nil if the object it is called on is nil, otherwise will return .word.

--- app/models/word.rb
+++ app/models/word.rb
@@ -3,8 +3,6 @@ class Word < ActiveRecord::Base
   scope :random_order, -> { order('RANDOM()') }
 
   def self.choose_word
-    rand_order = Word.random_order
-    return nil if rand_order.empty?
-    rand_order.first.word
+    Word.random_order.first&.word
   end
 end
