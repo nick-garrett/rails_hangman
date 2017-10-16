9b21f9644b95fe21e53bcd14d7e0e83b6f7d188c
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-17 08:50:55 +1300

    Validates that the length can only be one character. Even though the form can only enter one character, all business logic should also be in the model so that it can be reused elsewhere.

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,6 +1,7 @@
 class Guess < ActiveRecord::Base
   validates_uniqueness_of :letter, scope: :game_id
   validates_format_of :letter, with: /\A[a-zA-Z]+\z/
+  validates_length_of :letter, maximum: 1
 
   before_validation :downcase_letter
 
